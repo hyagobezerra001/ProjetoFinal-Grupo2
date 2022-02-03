5ae1d527b2451330bfa7329078b92e9162570a86
MaysaAlves <maysa.alves@webjump.com.br>
2022-02-03 10:07:09 -0300

    Temas Wine e Fashion criados

---
+++ app/design/frontend/webjump_themes/theme-wine/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/webjump_themes/theme-wine', __DIR__);

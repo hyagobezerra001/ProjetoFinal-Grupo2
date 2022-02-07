d4b06776faa05edccc952bb801db81e85b03acf5
leetsantos <leticia.silva@webjump.com.br>
2022-02-07 11:08:04 -0300

    Configurando Categorias

--- app/code/Webjump/Configuration/Setup/Patch/Data/CreateCategories.php
+++ app/code/Webjump/Configuration/Setup/Patch/Data/CreateCategories.php
@@ -9,18 +9,24 @@ use Magento\Catalog\Model\CategoryRepository;
 use Magento\Catalog\Model\Category;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\StoreRepository;
+
 
 class CreateCategories implements DataPatchInterface
 {
     private $category;
     private $repository;
     private $moduleDataSetup;
+    private $storeRepository;
+
 
     public function __construct (
         ModuleDataSetupInterface $moduleDataSetup,
         CategoryFactory $category,
-        CategoryRepository $repository
+        CategoryRepository $repository,
+        StoreRepository $storeRepository
     ){
+        $this->storeRepository = $storeRepository;
         $this->category = $category;
         $this->repository = $repository;
         $this->moduleDataSetup = $moduleDataSetup;
@@ -50,6 +56,13 @@ class CreateCategories implements DataPatchInterface
             ->setIncludeInMenu(true);
         $this->repository->save($fashion);
 
+        /* Setting as root category */
+        $store = $this->storeRepository->get('fashion');
+        $group = $store->getGroup();
+        $group->setRootCategoryId($fashion->getId());
+        $group->save();
+
+        /* Creating subcategories */
         $fashionNames = ['Roupas', 'Lingerie', 'Calçados', 'Acessórios', 'Promoções'];
         foreach ($fashionNames as $name) {
             $temp = $this->category->create();
@@ -70,10 +83,17 @@ class CreateCategories implements DataPatchInterface
             ->setPosition(3)
             ->setStoreId(3)
             ->setIncludeInMenu(true);
-        $this->repository->save($wine);
 
-      $wineNames = ['Vinhos', 'Espumantes', 'Premium', 'Kits'];
-      foreach ($wineNames as $name) {
+        /* Setting as root category */
+        $this->repository->save($wine);
+        $store = $this->storeRepository->get('wine');
+        $group = $store->getGroup();
+        $group->setRootCategoryId($wine->getId());
+        $group->save();
+
+        /* Creating subcategories */
+        $wineNames = ['Vinhos', 'Espumantes', 'Premium', 'Kits'];
+        foreach ($wineNames as $name) {
           $temp = $this->category->create();
           $temp->isObjectNew(true);
           $temp ->setName($name)

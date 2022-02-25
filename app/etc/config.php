6b6a8c303e00804cec525a576698a861f59f52f4
BrunoSavone <bruno.savone@webjump.com.br>
2022-02-25 09:55:25 -0300

    grid loja fashion

--- app/etc/config.php
+++ app/etc/config.php
@@ -297,6 +297,7 @@ return [
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
+        'Ulmod_Productslider' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
         'Webjump_Carbono' => 1,

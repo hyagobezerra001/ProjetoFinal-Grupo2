7da68d4de520abed4d738a35439cb71821d90fa7
BrunoSavone <bruno.savone@webjump.com.br>
2022-03-02 12:07:47 -0300

    alteração de modulo

---
+++ app/code/Ulmod/Productslider/Block/Widget/Slider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © Ulmod. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ulmod\Productslider\Block\Widget;
+
+class Slider extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Default template to use for slider widget
+     */
+    const DEFAULT_SLIDER_TEMPLATE = 'widget/slider.phtml';
+
+    /**
+     * set slider widget template
+     */
+    public function _construct()
+    {
+        if (!$this->hasData('template')) {
+            $this->setData('template', self::DEFAULT_SLIDER_TEMPLATE);
+        }
+        return parent::_construct();
+    }
+}
